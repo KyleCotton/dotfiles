6aafda3de9904abbed44342714dd6c0e2b899e33
Kyle Cotton <kylecottonkc@gmail.com>
2024-01-04 13:36:25 +0000

    Show hidden files in Oil directory view

--- nvim/.config/nvim/lua/kyle/plugins/oil.lua
+++ nvim/.config/nvim/lua/kyle/plugins/oil.lua
@@ -7,6 +7,13 @@ return {
         "permissions",
         "size",
         "mtime",
+      },
+      view_options = {
+        show_hidden = true,
+        -- Don't show the `../` file in the directory listing.
+        is_always_hidden = function(name, _)
+          return vim.startswith(name, "..")
+        end,
       }
     })
     vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory with Oil" })
