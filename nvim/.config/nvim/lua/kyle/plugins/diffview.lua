6882d5442b48aa67207c74d807875b3dd440196b
Kyle Cotton <kylecottonkc@gmail.com>
2024-04-21 13:54:00 +0100

    Add `diffview` plugin for nvim

---
+++ nvim/.config/nvim/lua/kyle/plugins/diffview.lua
@@ -0,0 +1,25 @@
+local diffview_toggle = function()
+  local diffview = require('diffview')
+  local current_diffview = next(require('diffview.lib').views)
+  if current_diffview == nil then
+    -- Not open so open it.
+    diffview.open()
+  else
+    -- Open so close it.
+    diffview.close()
+  end
+end
+
+return {
+  "sindrets/diffview.nvim",
+  config = function()
+    require('diffview').setup {
+      use_icons = false,
+    }
+
+    vim.keymap.set('n', '<leader>gd', diffview_toggle, { desc = 'Git [D]iff toggle' })
+
+    local set = vim.opt 
+    set.fillchars = set.fillchars + 'diff:╱'
+  end
+}
